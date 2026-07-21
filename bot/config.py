37d33de27941bea8097248d1f9572d99fe745581
deea77amer-creator <deea77amer@gmail.com>
2026-07-21 21:35:09 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,4 +6,3 @@ DISCORD_TOKEN = os.getenv("DISCORD_TOKEN", "YOUR_BOT_TOKEN_HERE")
 # رابط اتصال MongoDB Atlas الخاص بك (بدون تغيير)
 MONGO_URI = os.getenv("MONGO_URI", "YOUR_MONGO_URI_HERE")
 DATABASE_NAME = os.getenv("DATABASE_NAME", "YOUR_DATABASE_NAME_HERE")
-
