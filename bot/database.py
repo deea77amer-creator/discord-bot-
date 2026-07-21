42ca1cfbe9b187a0749ac6c1901d47011ebffaf2
deea77amer-creator <deea77amer@gmail.com>
2026-07-21 18:56:15 +0300

    Update database.py

--- bot/database.py
+++ bot/database.py
@@ -1,1 +1,11 @@
+import motor.motor_asyncio
+from bot.config import MONGO_URI, DATABASE_NAME
+
+# إنشاء عميل الاتصال الخاص بـ MongoDB
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
+db = client[DATABASE_NAME]
+
+async def get_database():
+    """إرجاع كائن قاعدة البيانات للاستخدام في باقي الملفات"""
+    return db
 
